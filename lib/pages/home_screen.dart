e8bdfff3553a04a8d2a242f3e37af522d2a9454d
zahid2873 <mahdi.zhm13@gmail.com>
2023-06-18 23:02:58 +0600

    api request

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../http_request/recipe_api.dart';
+import '../models/recipe_model.dart';
+import '../widgets/recipe_card.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -8,6 +12,22 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  late List<Recipe> _recipes;
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    getRecipes();
+  }
+
+  Future<void> getRecipes() async {
+    _recipes = await RecipeApi.getRecipe();
+    setState(() {
+      _isLoading = false;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,6 +43,17 @@ class _HomeScreenState extends State<HomeScreen> {
           ],
         ),
       ),
-    );
+      body:  _isLoading
+          ? Center(child: CircularProgressIndicator(color: Colors.blue,))
+          : ListView.builder(
+        itemCount: _recipes.length,
+        itemBuilder: (context, index) {
+          return RecipeCard(
+              title: _recipes[index].name,
+              cookTime: _recipes[index].totalTime,
+              rating: _recipes[index].rating.toString(),
+              thumbnailUrl: _recipes[index].images);
+        },
+      ));
   }
 }
